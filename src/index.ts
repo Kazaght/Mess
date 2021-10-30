915ea465f457955d09b173ad2ba55fdd63f78fcd
Kazaght <92064007+Kazaght@users.noreply.github.com>
2021-10-30 20:24:18 -0300

    tareaterminada Lunes

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,51 @@ let numerodemes: number = document.getElementById("numerodemes");
 let btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
 let numerodemes2= Number(numerodemes.value);
-let diasp: number = 2;
-let diass: number = 4 , 6, 9, 11;
-let diast: number = 1, 3, 5, 7, 8, 10, 12 ; 
-switch (true) {
-  case (numerodemes2===diasp):
-  console.log ("Tiene 28 dias");
-    
-    break; 
 
-    case (numerodemes2===diass):
-  console.log ("Tiene 30 dias");
+switch (numerodemes2) {
+case (2) :
+console.log ("Tiene 28 dias");
+    
+break; 
+case (11) :
+console.log ("Tiene 30 dias");
+          
+break; 
+case(4) :
+console.log ("Tiene 30 dias");
+break; 
+
+case (6) :
+console.log ("Tiene 30 dias");
+break; 
+
+case (9) :
+console.log ("Tiene 30 dias");
+break; 
+
+case (1) :
+console.log ("Tiene 30 dias");
     
     break;
-    case (numerodemes2===diast): 
+    case (3) : 
   console.log ("Tiene 31 dias");
     
-    break;
-
-  default:
-    break;
-}
+break;
+case (5) : 
+    console.log ("Tiene 31 dias");
+      
+break;
+case (7) : 
+     console.log ("Tiene 31 dias");
+       
+break;
+case (8) : 
+console.log ("Tiene 31 dias");
+          
+break;
+case (10) : 
+console.log ("Tiene 31 dias");
+              
+break;
+case (12) : 
+console.log ("Tiene 31 dias");
